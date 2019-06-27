65ebbd8b23d4da9200f776e7bb6e964d06c9db52
andremw96 <andre19961@gmail.com>
2019-06-27 21:38:24 +0700

    - hapus onbackpressed

--- app/src/main/java/com/andreamw96/moviecatalogue/activity/DetailMovieActivity.java
+++ app/src/main/java/com/andreamw96/moviecatalogue/activity/DetailMovieActivity.java
@@ -48,11 +48,6 @@ public class DetailMovieActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             finish();
