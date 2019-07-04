fbe8cf9497a5ca2f855bb692863e20b26c9987dd
andremw96 <andre19961@gmail.com>
2019-07-04 14:27:16 +0700

    - add progressbar frag movie, tvshow & move detail - add gson mode movies & tvshows

--- app/src/main/java/com/andreamw96/moviecatalogue/model/TvShows.kt
+++ app/src/main/java/com/andreamw96/moviecatalogue/model/TvShows.kt
@@ -1,7 +1,9 @@
 package com.andreamw96.moviecatalogue.model
 
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
 data class TvShows(
     @SerializedName("page")
@@ -14,31 +16,32 @@ data class TvShows(
     val totalResults: Int
 )
 
+@Parcelize
 data class TvResult(
-        @SerializedName("backdrop_path")
-        val backdropPath: String,
-        @SerializedName("first_air_date")
-        val firstAirDate: String,
-        @SerializedName("genre_ids")
-        val genreIds: List<Int>,
-        @SerializedName("id")
-        val id: Int,
-        @SerializedName("name")
-        val name: String,
-        @SerializedName("origin_country")
-        val originCountry: List<String>,
-        @SerializedName("original_language")
-        val originalLanguage: String,
-        @SerializedName("original_name")
-        val originalName: String,
-        @SerializedName("overview")
-        val overview: String,
-        @SerializedName("popularity")
-        val popularity: Double,
-        @SerializedName("poster_path")
-        val posterPath: String,
-        @SerializedName("vote_average")
-        val voteAverage: Double,
-        @SerializedName("vote_count")
-        val voteCount: Int
-)
+    @SerializedName("backdrop_path")
+    val backdropPath: String,
+    @SerializedName("first_air_date")
+    val firstAirDate: String,
+    @SerializedName("genre_ids")
+    val genreIds: List<Int>,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("origin_country")
+    val originCountry: List<String>,
+    @SerializedName("original_language")
+    val originalLanguage: String,
+    @SerializedName("original_name")
+    val originalName: String,
+    @SerializedName("overview")
+    val overview: String,
+    @SerializedName("popularity")
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    @SerializedName("vote_count")
+    val voteCount: Int
+) : Parcelable
