5f139a0bb9fd8bc287ae253ba6c15a0747a804f8
andremw96 <andre19961@gmail.com>
2019-07-27 20:34:51 +0700

    add function to bindview model

--- app/src/main/java/com/andreamw96/moviecatalogue/di/main/tvshows/TvShowsViewModelModule.kt
+++ app/src/main/java/com/andreamw96/moviecatalogue/di/main/tvshows/TvShowsViewModelModule.kt
@@ -1,2 +1,19 @@
 package com.andreamw96.moviecatalogue.di.main.tvshows
 
+import androidx.lifecycle.ViewModel
+import com.andreamw96.moviecatalogue.di.ViewModelKey
+import com.andreamw96.moviecatalogue.views.movies.list.MovieViewModel
+import com.andreamw96.moviecatalogue.views.tvshows.list.TvShowViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class TvShowsViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MovieViewModel::class)
+    abstract fun bindTvShowsViewModel(viewModel: TvShowViewModel) : ViewModel
+
+}
