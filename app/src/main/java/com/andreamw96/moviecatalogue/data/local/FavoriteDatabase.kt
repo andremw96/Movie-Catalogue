3db4fabccb4aee683192ea98e21076c0a7f7d761
andremw96 <andre19961@gmail.com>
2019-07-27 19:43:04 +0700

    create base application and appcomponent

--- app/src/main/java/com/andreamw96/moviecatalogue/data/local/FavoriteDatabase.kt
+++ app/src/main/java/com/andreamw96/moviecatalogue/data/local/FavoriteDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.andreamw96.moviecatalogue.data.model.Favorite
 
-@Database(entities = [Favorite::class], version = 2)
+@Database(entities = [Favorite::class], version = 2, exportSchema = false)
 abstract class FavoriteDatabase : RoomDatabase() {
 
     abstract fun favDao(): FavoriteDao
---
+++ app/src/main/java/com/andreamw96/moviecatalogue/di/AppComponent.kt
@@ -0,0 +1,30 @@
+package com.andreamw96.moviecatalogue.di
+
+import android.app.Application
+import com.andreamw96.moviecatalogue.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+
+@Singleton
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class
+    ]
+)
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application) : Builder
+
+        fun build() : AppComponent
+
+    }
+
+}
