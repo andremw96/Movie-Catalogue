c440754b55f164d021b6ea236d8c95620ee4ed64
andremw96 <andre19961@gmail.com>
2019-07-27 20:01:49 +0700

    create dependency for glide instance

--- app/src/main/java/com/andreamw96/moviecatalogue/di/AppModule.kt
+++ app/src/main/java/com/andreamw96/moviecatalogue/di/AppModule.kt
@@ -1,5 +1,10 @@
 package com.andreamw96.moviecatalogue.di
 
+import android.app.Application
+import com.andreamw96.moviecatalogue.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
 import dagger.Module
 import dagger.Provides
 
@@ -7,6 +12,16 @@ import dagger.Provides
 class AppModule {
 
     @Provides
-    fun someString() : String = "helloww"
+    fun provideRequestOptions() : RequestOptions {
+        return RequestOptions()
+                .placeholder(R.drawable.white_background)
+                .error(R.drawable.white_background)
+    }
+
+    @Provides
+    fun provideGlideInstance(application: Application, requestOptions: RequestOptions) : RequestManager {
+        return Glide.with(application)
+                .setDefaultRequestOptions(requestOptions)
+    }
 
 }
