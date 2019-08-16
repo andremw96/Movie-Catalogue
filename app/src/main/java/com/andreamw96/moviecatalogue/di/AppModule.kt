49dbca88b0c29c6a47ddd88461c0ed41348e893d
andremw96 <andre19961@gmail.com>
2019-08-16 20:04:42 +0700

    change force fetch data every 30sec

--- app/src/main/java/com/andreamw96/moviecatalogue/di/AppModule.kt
+++ app/src/main/java/com/andreamw96/moviecatalogue/di/AppModule.kt
@@ -93,7 +93,7 @@ class AppModule {
     @Singleton
     @Provides
     fun provideRateLimiter(): RateLimiter {
-        return RateLimiter(10, TimeUnit.SECONDS)
+        return RateLimiter(30, TimeUnit.SECONDS)
     }
 
     @Singleton
