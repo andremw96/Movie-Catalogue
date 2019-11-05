82e5686b5d4fb4d3612ac74fe3030301647524c2
andremw96 <andre19961@gmail.com>
2019-11-05 15:22:18 +0700

    create movierepository test

---
+++ app/src/test/java/com/andreamw96/moviecatalogue/data/source/remote/movie/MovieRepositoryTest.kt
@@ -0,0 +1,18 @@
+package com.andreamw96.moviecatalogue.data.source.remote.movie
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MovieRepositoryTest {
+
+    
+
+    @Test
+    fun getMoviesFromApi() {
+    }
+
+    @Test
+    fun getDetailMovieFromApi() {
+    }
+}
