79336beedf14e60596b0192219e115a49cdc978a
andremw96 <andre19961@gmail.com>
2019-11-12 16:31:12 +0700

    add appexecutors, apiresponse & networkboundresouece

---
+++ app/src/main/java/com/andreamw96/moviecatalogue/views/common/Resource.kt
@@ -0,0 +1,23 @@
+package com.andreamw96.moviecatalogue.views.common
+
+class Resource<T>(var status: Status, var data: T?, var message: String?) {
+
+    enum class Status {
+        SUCCESS, ERROR, LOADING
+    }
+
+    companion object {
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String, data: T?): Resource<T> {
+            return Resource(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+    }
+
+}
